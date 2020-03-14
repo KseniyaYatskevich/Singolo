0ce781bd5d4da59d14c4be08b9a4e7ab95d6a3bc
KseniaYatskevich <kseniayatskevich@gmail.com>
2020-03-14 16:03:07 +0300

    feat: add click on phones

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 window.onload = function () {
   addNavigationClickHandler();
+  addPhoneClickHandler();
 }
 
 const addNavigationClickHandler = () => {
   const navigation = document.getElementById('navigation');
-  navigation.addEventListener( 'click', (e) => {
+  navigation.addEventListener('click', (e) => {
     const target = e.target;
-    if(target.classList.contains('navigation__link')) {
+    if (target.classList.contains('navigation__link')) {
       navigation.querySelector('.active-menu').classList.remove('active-menu');
       target.classList.add('active-menu');
     }
   })
 }
+
+const addPhoneClickHandler = () => {
+  const sliderImage = document.querySelector('.slider__image');
+  sliderImage.addEventListener('click', (e) => {
+    const {parentNode} = e.target;
+    if (parentNode.classList.contains('phone_off')) {
+      parentNode.classList.remove('phone_off');
+    } else parentNode.classList.add('phone_off')
+  })
+};
